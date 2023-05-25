a85c0ff75e5167e11a0cfa63c6e7408508647a1e
Muhyammad Fajri Hadi Syahputra <87796259+HadiSyahputraa@users.noreply.github.com>
2023-05-25 20:34:52 +0700

    Organize file
    Making file so that easily track

--- Data/2_Conditioning.sql
+++ Data/2_Conditioning.sql
@@ -48,9 +48,11 @@ DROP COLUMN special_group
 --Saving Table
 Create View
 ASEAN_Generall_Grouped_Info_Cle as
-SELECT  ID, special_group, location, Date, Day, Month, Year, 
-		reproduction_rate, stringency_index, population_density, median_age, 
-		gdp_per_capita, extreme_poverty, life_expectancy, human_development_index, population
+SELECT  ID, special_group, location,
+		Date, Day, Month, Year,
+		reproduction_rate, stringency_index, population_density,
+		median_age, gdp_per_capita, extreme_poverty,
+		life_expectancy, human_development_index, population
 FROM ASEAN_Generall..Covid_Raw_Data_Clean$
 WHERE location = 'Brunei' or
 	  location = 'Cambodia' or
@@ -64,34 +66,15 @@ WHERE location = 'Brunei' or
 	  location = 'Vietnam'
 
 Create View
-COVID_Information as
+Comparison_PH_ID as
 SELECT  ID, new_tests, new_cases, new_deaths, positive_rate,	
 		total_cases, total_tests, total_deaths,	
 		new_tests_per_thousand,	total_tests_per_thousand,	
-		new_cases_per_million, new_deaths_per_million, total_deaths_per_million
-FROM ASEAN_Generall..Covid_Raw_Data_Clean$
-WHERE location = 'Indonesia' or location = 'Singapore' 
-
-Create View
-Hospital_Information as
-SELECT  ID, icu_patients, hosp_patients,	
+		new_cases_per_million, new_deaths_per_million, total_deaths_per_million,
+		icu_patients, hosp_patients,	
 		weekly_icu_admissions, weekly_hosp_admissions,	
 		hospital_beds_per_thousand,
-		icu_patients_per_million, hosp_patients_per_million, weekly_icu_admissions_per_million,	weekly_hosp_admissions_per_million	
+		icu_patients_per_million, hosp_patients_per_million, weekly_icu_admissions_per_million,	weekly_hosp_admissions_per_million,
+		handwashing_facilities, reproduction_rate
 FROM ASEAN_Generall..Covid_Raw_Data_Clean$
-WHERE location = 'Indonesia' or location = 'Singapore' 
-
-Create View
-Vaccine_Information as
-SELECT  ID, new_vaccinations, people_vaccinated, people_fully_vaccinated,	
-		people_vaccinated_per_hundred, people_fully_vaccinated_per_hundred, total_boosters_per_hundred,		
-		new_vaccinations_smoothed_per_million, new_people_vaccinated_smoothed, new_people_vaccinated_smoothed_per_hundred,
-		total_vaccinations,	total_boosters,	total_vaccinations_per_hundred	
-FROM ASEAN_Generall..Covid_Raw_Data_Clean$
-WHERE location = 'Indonesia' or location = 'Singapore'
-
-Create View
-misc_information as
-SELECT  ID, handwashing_facilities, reproduction_rate	
-FROM ASEAN_Generall..Covid_Raw_Data_Clean$
-WHERE location = 'Indonesia' or location = 'Singapore'
+WHERE location = 'Indonesia' or location = 'Philippines'
